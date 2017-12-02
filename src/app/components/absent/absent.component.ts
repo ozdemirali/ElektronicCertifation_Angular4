f09e5bff307a60575cd0e5669fbe20fbdc2e81f5
ozdemirali <ozdemirali@hotmail.com>
2017-12-02 16:52:14 +0300

    it is complated absentCourseStudentsHours

--- src/app/components/absent/absent.component.ts
+++ src/app/components/absent/absent.component.ts
@@ -1,6 +1,7 @@
 import { CourseDataService } from './../../services/course-data.service';
 import { Component, OnInit } from '@angular/core';
 import {Course} from '../../interfeces/course';
+import {CourseStudent} from '../../interfeces/courseStudent';
 import * as moment from 'moment';
 
 // import {CalendarModule} from 'primeng/primeng';
@@ -12,19 +13,46 @@ import { $ } from 'protractor';
   styleUrls: ['./absent.component.css']
 })
 export class AbsentComponent implements OnInit {
-  course:Course[];
+  courses:Course[];
+  courseStudents:CourseStudent[];
   courseId:number=1;
-  absent_date:string;
-  value:Date;
+  absent_date:Date=new Date();
   
   constructor(private courseDataService:CourseDataService) { }
 
   ngOnInit() {
     this.courseDataService.getCourseData().subscribe((data)=>{
-      this.course=data.result;
+      this.courses=data.result;
       //console.log(this.course);
-    });
+    })
+  }
 
+  findCourseStudents(){
+    this.courseDataService.getCourseStudents(this.courseId).subscribe((data)=>{
+        this.courseStudents=data.result;
+
+        for(let i in this.courseStudents){
+          this.courseStudents[i].hour=0;
+        }
+    })
+  }
+
+  saveCourseStudentsHours(){
+    let absentCourseStudentHours=[];
+    for(let i in this.courseStudents){
+        let item={
+          Student_id:this.courseStudents[i].id,
+          Course_id:this.courseId,
+          hour:this.courseStudents[i].hour,
+          day_date:moment(this.absent_date).format("DD/MM/YY")
+        };
+        absentCourseStudentHours.push(item);
+    }
+    console.log(absentCourseStudentHours);
+    this.courseDataService.postCourseStudentsHours(absentCourseStudentHours).subscribe((data)=>{
+      this.findCourseStudents();
+    })
+   
 
   }
 
--- src/app/services/course-data.service.ts
+++ src/app/services/course-data.service.ts
@@ -19,9 +19,21 @@ export class CourseDataService {
     //console.log(HttpAddress.getCourseDataAll);
   }
 
+  /*Tüm Kursları getirir */
   getCourseData(){
     return this.http.get(HttpAddress.getCourseDataAll.toString())
       .map(res=>res.json());
   }
+
+  /*Kusrlara ait öğrencileri getirir  */
+  getCourseStudents(id:number){
+    return this.http.get(HttpAddress.getCourseStudents.toString()+id)
+      .map(res=>res.json());
+  }
+
+  postCourseStudentsHours(item:Array<any>){
+    return this.http.post(HttpAddress.postCourseStudentsHours.toString(),item)
+      .map(res=>res.json());
+  }  
 
 }
